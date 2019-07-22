6b11ac0f571c89e740f7861cd4e26eb3f7cf8382
Ayush Raj <36340649+Ayushraj21@users.noreply.github.com>
2019-07-22 10:19:48 +0530

    Add files via upload

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -6,20 +6,23 @@
 """ Userbot initialization. """
 
 import os
-from distutils.util import strtobool as sb
-from logging import basicConfig, getLogger, INFO, DEBUG
-from sys import version_info
 
-import redis
+from sys import version_info
+from logging import basicConfig, getLogger, INFO, DEBUG
+from distutils.util import strtobool as sb
+
 from dotenv import load_dotenv
-from pymongo import MongoClient
 from requests import get
 from telethon import TelegramClient
+from telethon.sessions import StringSession
+from pymongo import MongoClient
+import redis
 
 load_dotenv("config.env")
 
 # Bot Logs setup:
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
 
 if CONSOLE_LOGGER_VERBOSE:
     basicConfig(
@@ -41,15 +44,15 @@ if version_info[0] < 3 or version_info[1] < 6:
     quit(1)
 
 # Check if the config was edited by using the already used variable
-CONFIG_CHECK = os.environ.get(
-    "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+CONFIG_CHECK = os.environ.get("___________PLOX_______REMOVE_____THIS_____LINE__________", None)
 
 if CONFIG_CHECK:
-    LOGS.error(
-        "Please remove the line mentioned in the first hashtag from the config.env file")
+    LOGS.error("Please remove the line mentioned in the first hashtag from the config.env file")
     quit(1)
 
 API_KEY = os.environ.get("API_KEY", None)
+
+STRING_SESSION = os.environ.get("STRING_SESSION", None)
 
 API_HASH = os.environ.get("API_HASH", None)
 
@@ -63,13 +66,13 @@ PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
 
 CONSOLE_LOGGER_VERBOSE = sb(
     os.environ.get("CONSOLE_LOGGER_VERBOSE", "False")
-)
+    )
 
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
 
 SCREENSHOT_LAYER_ACCESS_KEY = os.environ.get(
     "SCREENSHOT_LAYER_ACCESS_KEY", None
-)
+    )
 
 OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
 
@@ -79,7 +82,7 @@ WELCOME_MUTE = sb(os.environ.get(
 
 YOUTUBE_API_KEY = os.environ.get(
     "YOUTUBE_API_KEY", None
-)
+    )
 
 SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME", None)
 SPOTIFY_PASS = os.environ.get("SPOTIFY_PASS", None)
@@ -87,7 +90,7 @@ SPOTIFY_BIO_PREFIX = os.environ.get("SPOTIFY_BIO_PREFIX", None)
 DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
 
 # pylint: disable=invalid-name
-bot = TelegramClient("userbot", API_KEY, API_HASH)
+bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
 
 if os.path.exists("learning-data-root.check"):
     os.remove("learning-data-root.check")
@@ -103,145 +106,34 @@ with open('learning-data-root.check', 'wb') as load:
 MONGOCLIENT = MongoClient(MONGO_DB_URI, 27017, serverSelectionTimeoutMS=1)
 MONGO = MONGOCLIENT.userbot
 
-
 def is_mongo_alive():
     try:
         MONGOCLIENT.server_info()
-    except BaseException:
+    except:
         return False
     return True
-
-
 # Init Redis
-# Redis will be hosted inside the docker container that hosts the bot
-# We need redis for just caching, so we just leave it to non-persistent
-REDIS = redis.StrictRedis(host='localhost', port=6379, db=0)
+####### Redis will be hosted inside the docker container that hosts the bot
+####### We need redis for just caching, so we just leave it to non-persistent
 
+REDIS = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def is_redis_alive():
     try:
         REDIS.ping()
         return True
-    except BaseException:
+    except:
         return False
-
 
 # Global Variables
 COUNT_MSG = 0
 BRAIN_CHECKER = []
 USERS = {}
+WIDE_MAP = dict((i, i + 0xFEE0) for i in range(0x21, 0x7F))
+WIDE_MAP[0x20] = 0x3000
 COUNT_PM = {}
 LASTMSG = {}
 ENABLE_KILLME = True
 CMD_HELP = {}
 AFKREASON = "no reason"
-ZALG_LIST = [["̖",
-              " ̗",
-              " ̘",
-              " ̙",
-              " ̜",
-              " ̝",
-              " ̞",
-              " ̟",
-              " ̠",
-              " ̤",
-              " ̥",
-              " ̦",
-              " ̩",
-              " ̪",
-              " ̫",
-              " ̬",
-              " ̭",
-              " ̮",
-              " ̯",
-              " ̰",
-              " ̱",
-              " ̲",
-              " ̳",
-              " ̹",
-              " ̺",
-              " ̻",
-              " ̼",
-              " ͅ",
-              " ͇",
-              " ͈",
-              " ͉",
-              " ͍",
-              " ͎",
-              " ͓",
-              " ͔",
-              " ͕",
-              " ͖",
-              " ͙",
-              " ͚",
-              " ",
-              ],
-             [" ̍",
-              " ̎",
-              " ̄",
-              " ̅",
-              " ̿",
-              " ̑",
-              " ̆",
-              " ̐",
-              " ͒",
-              " ͗",
-              " ͑",
-              " ̇",
-              " ̈",
-              " ̊",
-              " ͂",
-              " ̓",
-              " ̈́",
-              " ͊",
-              " ͋",
-              " ͌",
-              " ̃",
-              " ̂",
-              " ̌",
-              " ͐",
-              " ́",
-              " ̋",
-              " ̏",
-              " ̽",
-              " ̉",
-              " ͣ",
-              " ͤ",
-              " ͥ",
-              " ͦ",
-              " ͧ",
-              " ͨ",
-              " ͩ",
-              " ͪ",
-              " ͫ",
-              " ͬ",
-              " ͭ",
-              " ͮ",
-              " ͯ",
-              " ̾",
-              " ͛",
-              " ͆",
-              " ̚",
-              ],
-             [" ̕",
-              " ̛",
-              " ̀",
-              " ́",
-              " ͘",
-              " ̡",
-              " ̢",
-              " ̧",
-              " ̨",
-              " ̴",
-              " ̵",
-              " ̶",
-              " ͜",
-              " ͝",
-              " ͞",
-              " ͟",
-              " ͠",
-              " ͢",
-              " ̸",
-              " ̷",
-              " ͡",
-              ]]
+DISABLE_RUN = False
